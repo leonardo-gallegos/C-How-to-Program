2d52545acdd437e3338ae5d499adaf514158244c
unknown <renzogallegos96@gmail.com>
2023-03-10 23:02:20 -0500

    Added class AccountTest1.cpp

---
+++ Chapter 3/AccountTest1.cpp
@@ -0,0 +1,17 @@
+// Fig. 3.5: AccountTest.cpp
+// Using the Account constructor to initialize the name data
+// member at the tiem each Account object is created.
+#include <iostream>
+#include "Account1.h"
+
+using namespace std;
+
+int main() {
+    // create two Account objects
+    Account account1{"Jane Green"};
+    Account account2{"John Blue"};
+
+    // display initial value of name for each Account
+    cout << "account1 name is: " << account1.getName() << endl;
+    cout << "account2 name is: " << account2.getName() << endl;
+}
