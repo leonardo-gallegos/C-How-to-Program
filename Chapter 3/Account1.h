e7a2eeedb771944dc21ed65dbf94813c2ef3b60e
unknown <renzogallegos96@gmail.com>
2023-03-10 22:31:37 -0500

    Added class Account1.h

---
+++ Chapter 3/Account1.h
@@ -0,0 +1,24 @@
+// Fig. 3.4: Account.h
+// Account class with a constructor that initializes the account anme.
+#include <string>
+
+class Account {
+public:
+    // constructor initializes data member name with parameter accountName
+    explicit Account(std::string accountName)
+      : name{accountName} { // member initializer
+      // empty body 
+    }
+
+    // function to set the account name
+    void setName(std::string accountName) {
+        name = accountName;
+    }
+
+    // fcuntion to retrieve the accpunt name
+    std::string getName() const {
+        return name;
+    }
+private:
+    std::string name; // account name data member
+}; // end class Account
