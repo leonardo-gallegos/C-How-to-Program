8a8439f3b7382e0be135ac33e521c7174a55c024
unknown <renzogallegos96@gmail.com>
2023-03-11 23:29:41 -0500

    Modified AccountTest.cpp

--- Chapter 3/Fig. 3.5/AccountTest.cpp
+++ Chapter 3/Fig. 3.5/AccountTest.cpp
@@ -3,3 +3,15 @@
 // member at the time each Account object is created
 #include <iostream>
 #include "Account.h"
+
+using namespace std;
+
+int main() {
+    // Create two Account objects
+    Account account1{"Jane Green"};
+    Account account2{"John Blue"};
+
+    // display initial value of name for each Account
+    cout << "account1 name is: " << account1.getName() << endl;
+    cout << "account2 name is: " << account2.getName() << endl;
+}
