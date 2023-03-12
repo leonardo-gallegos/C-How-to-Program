2b5fe7505e6e238bda6825be338b7905917d611e
unknown <renzogallegos96@gmail.com>
2023-03-12 00:02:31 -0500

    Modified Account.h

--- Chapter 3/Fig. 3.8/Account.h
+++ Chapter 3/Fig. 3.8/Account.h
@@ -6,4 +6,26 @@
 class Account {
 public:
     // Account constructor with two parameters
+    Account(std::string accountName, int initialBalance)
+        : name{accountName} { // assign accountName to data member name
+
+        // validate that the initialBalance is greater than 0; if not,
+        // data member balance keeps its defalut initial value of 0
+        if (initialBalance > 0) { // if the initialBalance is valid
+            balance = initialBalance; // assign it to data member balance
+        }
+    }
+
+    // function that deposits (adds) only a valid amount to the balance
+    void deposit(int depositAmount) {
+        if (depositAmount > 0) {
+            balance = balance + depositAmount; // add it to the balance
+        }
+    }
+
+    // function returns the account balance
+    
+private:
+    std::string name;
+    int balance{0};
 };
