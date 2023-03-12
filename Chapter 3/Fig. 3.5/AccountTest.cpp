3f75f7a927ab91377a0f61b6b2acf6d498d3240a
unknown <renzogallegos96@gmail.com>
2023-03-11 23:24:48 -0500

    Added Fig. 3.5

---
+++ Chapter 3/Fig. 3.5/AccountTest.cpp
@@ -0,0 +1,5 @@
+// Fig. 3.5: AccountTest.cpp
+// Using the Account constructor to initialize the name data
+// member at the time each Account object is created
+#include <iostream>
+#include "Account.h"
