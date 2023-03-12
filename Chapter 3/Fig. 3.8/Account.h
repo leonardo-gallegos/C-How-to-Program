99dbd52dc0892ca14d809e8cca270a3c8111d3ea
unknown <renzogallegos96@gmail.com>
2023-03-11 23:49:08 -0500

    Modified Account.h

--- Chapter 3/Fig. 3.8/Account.h
+++ Chapter 3/Fig. 3.8/Account.h
@@ -1,1 +1,9 @@
 // Fig. 3.8: Account.h
+// Account class with name and balance data members, and a
+// constructor and deposit fucntion that each perform validation.
+#include <string>
+
+class Account {
+public:
+    // Account constructor with two parameters
+};
