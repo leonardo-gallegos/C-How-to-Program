783459d78148191c37c9ea3a639fef8fa14c4d23
unknown <renzogallegos96@gmail.com>
2023-03-13 00:04:22 -0500

    Modified AccountTest.cpp

--- Chapter 3/Fig. 3.8/AccountTest.cpp
+++ Chapter 3/Fig. 3.8/AccountTest.cpp
@@ -9,4 +9,10 @@ int main()
 {
     Account account1{"Jane Green", 50};
     Account account2{"John Blue", -7};
+
+    // display initial balance of each object
+    cout << "account1: " << account1.getName() << " balance is $"
+        << account1.getBalance();
+    cout << "account2: " << account2.getName() << " balance is $"
+        << account2.getBalance();
 }
