86689fb3dda487423722438624ec3ec4be6f290e
leonardo-gallegos <renzogallegos96@gmail.com>
2023-03-15 16:35:54 -0500

    Fixed typo

--- Chapter 3/Fig. 3.8/AccountTest.cpp
+++ Chapter 3/Fig. 3.8/AccountTest.cpp
@@ -43,6 +43,6 @@ int main()
 /*
 The main function in Fig. 3.9 creates two Account objects (lines 10–11) and attempts to
 initialize them with a valid balance of 50 and an invalid balance of -7, respectively—for
-the purpose of our examples, we assume that balances must be greater than or equal to zero. Lines 14–17 output the account names and balances, which are obtained by calling
+the purpose of our examples, we assume that balances must be greater than or equal to zero. Lines 14–17 output the account names and balances, which are obtained by calling
 each Account’s getName and getBalance member functions.
 */
